13350db45e251dc20a128722ea90c532ca567f4e
georgeg <georgegi86@gmail.com>
2024-12-30 22:52:08 -0600

    added export functions

--- src/BackTestAnalytics.jl
+++ src/BackTestAnalytics.jl
@@ -5,6 +5,12 @@ using DataFrames
 using StatsBase
 using Plots, GR
 
+export rows_spearmanr, rows_pearsonr, spearman_factor_decay 
+export mean_autocor, rolling_mean_autocor
+export plot_factor_distribution, plot_performance_table
+export quantile_return, compute_quantiles_returns,
+export quantile_turnover, quantiles_turnover, total_quantiles_turnover, quantile_performance_table, quantile_chg
+
 # Write your package code here.
 
     include("infocoef_analytics.jl")
