2c220eb3b58c88eda1f1dc8c72958e00ad291a2b
georgeg <georgegi86@gmail.com>
2024-12-30 23:06:05 -0600

    Update BackTestAnalytics.jl

--- src/BackTestAnalytics.jl
+++ src/BackTestAnalytics.jl
@@ -5,11 +5,10 @@ using DataFrames
 using StatsBase
 using Plots, GR
 
-export rows_spearmanr, rows_pearsonr, spearman_factor_decay 
-export mean_autocor, rolling_mean_autocor
-export plot_factor_distribution, plot_performance_table
-export quantile_return, compute_quantiles_returns,
-export quantile_turnover, quantiles_turnover, total_quantiles_turnover, quantile_performance_table, quantile_chg
+export rows_spearmanr, rows_pearsonr, spearman_factor_decay, mean_autocor, rolling_mean_autocor,
+    plot_factor_distribution, plot_performance_table, 
+    quantile_return, compute_quantiles_returns, 
+    quantile_turnover, quantiles_turnover, total_quantiles_turnover, quantile_performance_table, quantile_chg
 
 # Write your package code here.
 
